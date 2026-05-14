14ed595fe16504b5a46fbb8ae24c9326f948cba9
vivekkarthik16-tech <vivekkarthik16@gmail.com>
2026-05-14 16:40:58 +0530

    project completed

--- JavaScriptProject/script.js
+++ JavaScriptProject/script.js
@@ -28,7 +28,7 @@ if (signupForm) {
         localStorage.setItem("user", JSON.stringify(user));
 
         alert("Singedup Successfuly ");
-        window.location.href="login.html";
+        window.location.href = "login.html";
     });
 }
 
@@ -59,12 +59,112 @@ if (loginForm) {
 //javascript for changing the heading after login
 let heading = document.getElementById("welcomeMessage");
 
-if(heading){
+if (heading) {
     //firstt get theuser details
     let user = JSON.parse(localStorage.getItem("user"));
-    
+    let isLoggedIn = JSON.parse(localStorage.getItem("isLoggedIn"));
+
     //if the user exists then change the welcome message
-    if(user){
+    if (user && isLoggedIn) {
         heading.innerHTML = "Welcome " + user.name + "!...";
     }
+}
+
+//protecting the TASk.html from opening without log in 
+let currentPage = window.location.pathname;
+
+if (currentPage.includes("task.html")) {
+    let loginStatus = JSON.parse(localStorage.getItem("isLoggedIn"));
+
+    if (!loginStatus) {
+        alert("please log in first");
+        window.location.href = "login.html";
+    }
+}
+
+//Logout btn logic
+
+let LogoutButton = document.getElementById("logoutBtn");
+
+if (LogoutButton) {
+    LogoutButton.addEventListener("click", function () {
+        localStorage.removeItem("isLoggedIn");
+        alert("Logged out successful!..");
+        window.location.href = "index.html"
+    });
+
+}
+
+//Dynamic NavBar Logic
+
+let signupLink = document.getElementById("signuplink");
+let loginLink = document.getElementById("loginlink");
+let logoutBtn = document.getElementById("logoutBtn");
+
+// checking if the login is true or false!
+
+let loginStatus = localStorage.getItem("isLoggedIn");
+
+if(loginStatus == "true"){
+    if(signupLink){
+        signupLink.style.display = "none";
+    }
+    if(loginLink){
+        loginLink.style.display = "none";
+    }
+}else{
+    if(logoutBtn){
+        logoutBtn.style.display = "none";
+    }
+}
+
+// Dynamycally adding Tasks in the task.html 
+let addButton = document.getElementById("addTaskBtn");
+
+if(addButton) {
+    showTasks();
+    addButton.addEventListener("click", function(){
+        let task = document.getElementById("taskInput").value;
+        if (task == ""){
+            alert("Enter the task first!..");
+            return;
+        };
+        // 
+        let taskArray = JSON.parse(localStorage.getItem("task")) || [];
+        taskArray.push(task)
+        localStorage.setItem("task",JSON.stringify(taskArray));
+        taskInput.value="";
+        showTasks();
+    });
+}
+
+function showTasks(){
+    let taskList = document.getElementById("taskList");
+    if(!taskList){
+        return ;
+    }
+
+    taskList.innerHTML = "";
+    let taskArray = JSON.parse(localStorage.getItem("task")) || [];
+
+    for(let i=0; i<taskArray.length; i++){
+        taskList.innerHTML += `
+        <tr>
+            <td>${taskArray[i]}</td>
+            <td>
+                <button onClick="deleteTask(${i})">Delete</Button>
+            </td>
+        </tr>
+        `;
+    }
+}
+function deleteTask(index){
+
+    let taskArray = JSON.parse(localStorage.getItem("task")) || [];
+
+    taskArray.splice(index, 1);
+
+    localStorage.setItem("task", JSON.stringify(taskArray));
+
+    showTasks();
 }
